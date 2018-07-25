3453cc0f81e836767824c8a078ec14646f9107c6
Paige Wettroth <pwettroth@gmail.com>
2018-07-24 23:43:26 -0500

    Initial commit

---
+++ src/main/java/com/simplycindy/simplycindy/SimplyCindyApplication.java
@@ -0,0 +1,12 @@
+package com.simplycindy.simplycindy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyCindyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplyCindyApplication.class, args);
+	}
+}
